d170aee6d57a7bd6975cdf08db7f06a4e5264ace
Owais <blackhawk2399@gmail.com>
2021-04-09 12:09:24 +0500

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import './App.css';
-import LandingPage from './components/LandingPage'
-import SignUpDoctor from './components/SignUpDoctor'
-import SignUpPharmacist from './components/SignUpPharmacist'
+import "./App.css";
+import LandingPage from "./components/LandingPage";
+import SignUpDoctor from "./components/SignUpDoctor";
+import SignUpPharmacist from "./components/SignUpPharmacist";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return (
-    <SignUpPharmacist></SignUpPharmacist>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route path="/signUpDoctor">
+					<SignUpDoctor></SignUpDoctor>
+				</Route>
+				<Route path="/signUpPharmacist">
+					<SignUpPharmacist></SignUpPharmacist>
+				</Route>
+				<Route path="/">
+					<LandingPage></LandingPage>
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function LandingPage() {
-    return (
-        <div id="root-container">
-            <div id="heading-container">
-                <h2>Welcome!<br/>Who are you?</h2>
-            </div>
-            <div id="buttons-container">
-                <button className="landing-page-button">Doctor</button>
-                <button className="landing-page-button">Pharmacist</button>
-            </div>
-        </div>
-    )
+
+    const styles = {
+        link: {
+            textDecoration: "inherit",
+            color: "inherit"
+        }
+    }
+
+	return (
+		<div id="root-container">
+			<div id="heading-container">
+				<h2>
+					Welcome!
+					<br />
+					Who are you?
+				</h2>
+			</div>
+			<div id="buttons-container">
+				<button className="landing-page-button">
+					<Link to="/signUpDoctor" style={styles.link}>Doctor</Link>
+				</button>
+				<button className="landing-page-button">
+					<Link to="/signUpPharmacist" style={styles.link}>Pharmacist</Link>
+				</button>
+			</div>
+		</div>
+	);
 }
 
-export default LandingPage
+export default LandingPage;
